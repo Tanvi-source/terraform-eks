d6946f4b467f4d5487d9b61c1e21aaa732fa818d
Tanvi-source <tanvibhusari2003@gmail.com>
2025-10-19 07:49:51 +0530

    Add EKS module parameters for production environment

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,3 +1,11 @@
 module "eks" {
   source           = "../../modules/eks"
   cluster_name     = var.cluster_name
+  subnet_ids       = var.subnet_ids
+  cluster_role_arn = var.cluster_role_arn
+  node_role_arn    = var.node_role_arn
+  node_type        = var.node_type
+  desired_nodes    = var.desired_nodes
+  max_nodes        = var.max_nodes
+  min_nodes        = var.min_nodes
+}
