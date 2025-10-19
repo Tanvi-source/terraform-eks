ace33f578ed8efde2a7559fc10942319e354ad03
Tanvi-source <tanvibhusari2003@gmail.com>
2025-10-19 07:45:13 +0530

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,11 +1,3 @@
 module "eks" {
   source           = "../../modules/eks"
   cluster_name     = var.cluster_name
-  subnet_ids       = var.subnet_ids
-  cluster_role_arn = var.cluster_role_arn
-  node_role_arn    = var.node_role_arn
-  node_type        = var.node_type
-  desired_nodes    = var.desired_nodes
-  min_nodes        = var.min_nodes
-  max_nodes        = var.max_nodes
-}
